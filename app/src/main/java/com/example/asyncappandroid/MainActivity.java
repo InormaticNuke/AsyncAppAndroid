d0fb7c80ce32d8041fb5ed7901efea211f9139b4
Nuke <InformaticNuke@gmail.com>
2023-10-03 21:05:16 -0300

    v0.003

--- app/src/main/java/com/example/asyncappandroid/MainActivity.java
+++ app/src/main/java/com/example/asyncappandroid/MainActivity.java
@@ -2,13 +2,44 @@ package com.example.asyncappandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.txtLdng);
+        imageView = findViewById(R.id.imageView);
+
+        MyAsyncTask asyncTask = new MyAsyncTask();
+        asyncTask.execute();
+    }
+
+    public class MyAsyncTask extends AsyncTask<Void, Void, String>{
+
+        protected String doInBackground(Void... voids){
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
+            return "Completed Process";
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            textView.setText(result);
+            imageView.setVisibility(imageView.VISIBLE);
+        }
     }
 }
